6e83de54a32eabd347a31d9a184685f321167ce3
PC-20230109IYOH\Administrator <muliminty.zheng@zkteco.com>
2024-04-19 16:35:40 +0800

    feat: 使用配置文件生成

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const { readTemplateFile, generatedTemplateFile, } = require('./src/utils/renderTemplateFile')
+const { templates } = require('./src/config')
 
-const UserTable = require('./src/model/user.json');
 
+const processTemplates = () => {
 
-const templateContent = readTemplateFile({
-  targetPath: `${__dirname}/src/template/web`,
-  name: 'proTable.txt'
-})
-// 使用 art-template 渲染模板
-const generatedCode = generatedTemplateFile({
-  templateContent: templateContent,
-  data: UserTable
-})
+  templates.forEach((i) => {
+    const templateContent = readTemplateFile({
+      targetPath: `${__dirname}${i.outPath}`,
+      name: i.templateName
+    })
+    // 使用 art-template 渲染模板
+    const generatedCode = generatedTemplateFile({
+      templateContent: templateContent,
+      data: i.dataSource
+    })
 
-// 创建 output 文件夹（如果不存在）
-const outputDir = path.join(__dirname, 'output/template');
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
+    // 创建 output 文件夹（如果不存在）
+    const outputDir = path.join(__dirname, `${i.targetPath}`);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
+
+    // 将生成的代码写入文件
+    const outputPath = path.join(outputDir, `${[i.dataSource.tableName]}.js`);
+    fs.writeFileSync(outputPath, generatedCode, 'utf-8');
+
+    console.log('代码已生成:', outputPath);
+  })
+
 }
 
-// 将生成的代码写入文件
-const outputPath = path.join(outputDir, `${UserTable.tableName}.js`);
-fs.writeFileSync(outputPath, generatedCode, 'utf-8');
-
-console.log('代码已生成:', outputPath);
+processTemplates()
--- src/config.js
+++ src/config.js
@@ -1,1 +1,18 @@
+const UserTable = require('../src/model/user.json');
+
+
 // 配置文件
+module.exports = {
+  templates: [
+    // 前端模板
+    {
+      "templateName": "proTable.txt",
+      "outSuffix": ".js",
+      "outPath": "/src/template/web",
+      'targetPath': 'output/template',
+      "dataSource": UserTable
+      //覆盖文件
+      // overwrite: true
+    }
+  ]
+};
